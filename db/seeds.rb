f94bd9c72fba34cc081eb8882c24283414252bff
JANKEE92PL <jankee112@freenet.de>
2022-01-19 22:57:18 +0100

    initializer added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
     description:      Faker::Restaurant.description,
     price_per_night:  rand(1..100),
     number_of_guests: rand(1..5),
-    picture:          Unsplash::Photo.search('architecture', 1, 25)
+    picture:          Unsplash::Photo.search("cats")
   )
 end
