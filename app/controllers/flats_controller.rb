c29c5bd9781eba35dd884e020812926ddd453c55
JANKEE92PL <jankee112@freenet.de>
2022-01-19 21:59:58 +0100

    finish new

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,18 +1,27 @@
 class FlatsController < ApplicationController
-  before_action :find_flat, only: [:new]
+  # before_action :find_flat, only: [:new]
 
   def index
     @flats = Flat.all
   end
 
   def new
-    @flat = Flat.new
+    @flat = Flat.new()
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path(@flat)
+  end
 
   private
 
   def find_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
