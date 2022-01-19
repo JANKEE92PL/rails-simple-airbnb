b7d6a98cf49d4ca416d0107d892a203cc960df6f
JANKEE92PL <jankee112@freenet.de>
2022-01-19 21:06:17 +0100

    add new view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,18 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [:new]
+
   def index
     @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
   end
 end
