0db71713d356381e1f5de31d0f02e676a7678948
JANKEE92PL <jankee112@freenet.de>
2022-01-19 22:48:20 +0100

    update seeds

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,9 @@
+Unsplash.configure do |config|
+  config.application_access_key = "oO-itdYuJlgXFYNYtHLX2C53utWX28RcDStk_ZjVEY0"
+  config.application_secret = "rve9TZMq9HL-hKCYws0PKTCk6DWMQ7dEkqMB8Xk3cb0"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  config.utm_source = "Air Bnb Clone Learning Ruby on Rails with LeWagon"
+
+  # optional:
+  config.logger = MyCustomLogger.new
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
     description:      Faker::Restaurant.description,
     price_per_night:  rand(1..100),
     number_of_guests: rand(1..5),
-    picture:          Faker::LoremFlickr.image
+    picture:          Unsplash::Photo.search('architecture', 1, 25)
   )
 end
