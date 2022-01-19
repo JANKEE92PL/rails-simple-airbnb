a8cd9b12c90a4fc7600ad3733189e0cf6ef12fb9
JANKEE92PL <jankee112@freenet.de>
2022-01-19 16:32:14 +0100

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-10.times do Flat.create!(
-  name: Faker::Artist.name,
-  address: Faker::Address.full_address,
-  description: Faker::Restaurant.description,
-  price_per_night: rand(1..100),
-  number_of_guests: rand(1..5)
-)
+10.times do
+  Flat.create!(
+    name:             Faker::Artist.name,
+    address:          Faker::Address.full_address,
+    description:      Faker::Restaurant.description,
+    price_per_night:  rand(1..100),
+    number_of_guests: rand(1..5)
+  )
+end
