60dd8d7165c8077b4eb434206b6af15ca1905184
JANKEE92PL <jankee112@freenet.de>
2022-01-20 15:06:38 +0100

    finish sortablejs

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -8,13 +8,17 @@
 // </div>
 
 import {Controller} from "stimulus"
-import Sortable from 'sortablejs'
+import {Sortable} from 'sortablejs'
+
 export default class extends Controller {
     connect() {
         this.sortable = Sortable.create(this.element, {
+            ghostClass: 'selected',
+            animation: 200,
             onEnd: this.end.bind(this)
         })
     }
+
     end(event) {
         console.log(event)
     }
