6e590f97aa34626e3675f3be3cdf7762622a2fdf
JANKEE92PL <jankee112@freenet.de>
2022-01-20 14:38:37 +0100

    add Gem draggable-rails and config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require('jquery')
+//= require draggable
+
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -9,3 +13,5 @@ import "channels"
 
 Rails.start()
 Turbolinks.start()
+
+import "controllers"
